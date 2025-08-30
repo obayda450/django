e46478861ecc8854f223b4710fd96d00d941f56b
obayda450 <145752353+obayda450@users.noreply.github.com>
2025-08-30 12:45:56 +0300

    first edit

--- treaning/settings.py
+++ treaning/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-70!+(8)-4w6i@lwvau@kfk#fjo_d=d+ez!89gvp)eje-!lunn-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["obayda450.pythonanywhere.com"]
 
 
 # Application definition
@@ -124,6 +125,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-STATIC_URL = 'static/'  # غالباً عندك موجودة
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
